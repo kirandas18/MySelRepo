75559bff51e2a241ae8658f10c37975cfe42375f
kgaddam <kgaddam@INHYNKGADDAM2.infor.com>
2020-04-05 01:03:12 +0530

    Uploading files in local

---
+++ MyAprMavenArtTest/src/main/java/testPackage/TestMavenFirst.java
@@ -0,0 +1,20 @@
+package testPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class TestMavenFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Kiran\\Gecko\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		System.out.println(driver.getTitle());
+		System.out.println("Hello World");
+		driver.close();
+
+	}
+
+}
